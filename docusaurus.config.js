9675f3a9b245f85c58812ce0a7daced653cc27a6
qwertyoze <roangel474@gmail.com>
2024-09-29 00:02:52 +0300

    change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My page',
-  tagline: 'I am cool',
+  tagline: 'I am cool!!!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
